de56a0c8685a8b024a3098cc66fb5ecc212e541d
Rohit Jagdale <rohitjagadale4@gmail.com>
2022-02-15 19:52:15 +0530

    incomplete auth google integarate

--- app/src/main/java/com/taskmanager/horkrux/Authentication/LoginAndSignUp.java
+++ app/src/main/java/com/taskmanager/horkrux/Authentication/LoginAndSignUp.java
@@ -1,12 +1,41 @@
 package com.taskmanager.horkrux.Authentication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+import com.taskmanager.horkrux.Activites.MainActivity;
+import com.taskmanager.horkrux.Models.Users;
 import com.taskmanager.horkrux.R;
 
 public class LoginAndSignUp extends AppCompatActivity {
@@ -16,16 +45,27 @@ public class LoginAndSignUp extends AppCompatActivity {
     FloatingActionButton fb, google;
     float v = 0;
 
+    Users users;
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    private FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+
+    private static int RC_SIGN_IN = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_and_sign_up);
 
+//        Initialization
         tabLayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.view_pager);
-        fb = findViewById(R.id.fab_fb);
         google = findViewById(R.id.fab_google);
 
+
+//        TabLayout
         tabLayout.addTab(tabLayout.newTab().setText("Login"));
         tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
@@ -35,17 +75,88 @@ public class LoginAndSignUp extends AppCompatActivity {
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-        fb.setTranslationY(300);
+        //animation
+
         google.setTranslationY(300);
         tabLayout.setTranslationY(300);
 
-        fb.setAlpha(v);
+
         google.setAlpha(v);
         tabLayout.setAlpha(v);
 
-        fb.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+
         google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
         tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
 
+
+//        google Integration
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult.launch(signInIntent);
+            }
+        });
+
     }
+
+    ActivityResultLauncher<Intent> startActivityForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                        handleSignInResult(task);
+                    }
+                }
+            }
+    );
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            if (account != null) {
+
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+                firebaseAuth.signInWithCredential(credential)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Log.d("TAG", "onComplete: sucess");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                            users.setUserEmail(user.getEmail());
+                            users.setUserName(user.getDisplayName());
+                            users.setFireuserid(user.getUid());
+
+
+
+                            startActivity(new Intent(LoginAndSignUp.this,MainActivity.class));
+                            finish();
+                        }
+                    }
+                });
+
+            }
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.d("TAG", "handleSignInResult: ");
+        }
+    }
+
+
 }
