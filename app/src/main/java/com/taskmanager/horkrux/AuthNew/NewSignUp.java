858626f5f28e99e4b773de744bf267d0fe13a531
MusheerAhmed <musheerjamadar1024@gmail.com>
2022-02-26 01:04:41 +0530

    added field to database

--- app/src/main/java/com/taskmanager/horkrux/AuthNew/NewSignUp.java
+++ app/src/main/java/com/taskmanager/horkrux/AuthNew/NewSignUp.java
@@ -1,13 +1,14 @@
 package com.taskmanager.horkrux.AuthNew;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,15 +24,15 @@ public class NewSignUp extends AppCompatActivity {
 
     private ActivityNewSignUpBinding binding;
 
-    final String[] taskCategories = {"Android Dev", "Web Dev", "UI/UX", "MBA"};
+    final String[] deptCategory = {Users.ANDROID_DEPT, Users.WEB_DEPT, Users.UI_UX_DEPT, Users.MBA_DEPT};
     final int Android_Dev = 0, Web_Dev = 1, UI_UX = 2, MBA = 3;
 
     private ArrayAdapter fieldCategoryAdapter;
 
     private FirebaseAuth firebaseAuth;
-    FirebaseDatabase firebaseDatabase;
+    private FirebaseDatabase firebaseDatabase;
 
-    Users user = new Users();
+    private Users user;
 
 
     @Override
@@ -43,11 +44,19 @@ public class NewSignUp extends AppCompatActivity {
         //firebase declarations
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
+        user = new Users();
 
 
 //        load field
-        fieldCategoryAdapter = new ArrayAdapter(getApplicationContext(), R.layout.home_list_item, taskCategories);
+        fieldCategoryAdapter = new ArrayAdapter(getApplicationContext(), R.layout.home_list_item, deptCategory);
         binding.createUserField.setAdapter(fieldCategoryAdapter);
+
+        binding.createUserField.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                user.setUserDept(deptCategory[position]);
+            }
+        });
 
 
         binding.createUserLoginButton.setOnClickListener(new View.OnClickListener() {
@@ -58,35 +67,33 @@ public class NewSignUp extends AppCompatActivity {
                 String password = binding.createUserPass.getText().toString().trim();
                 String username = binding.crateUserName.getText().toString().trim();
 
-                if(email.isEmpty() || password.isEmpty() || username.isEmpty())
-                {
-                    Toast.makeText(getApplicationContext(),"Fill All Fields",Toast.LENGTH_SHORT).show();
-                }
-                else if (password.length() < 7) {
+                if (email.isEmpty() || password.isEmpty() || username.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Fill All Fields", Toast.LENGTH_SHORT).show();
+                } else if (password.length() < 7) {
 
-                    Toast.makeText(getApplicationContext(),"Must be 8 characters or more",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Must be 8 characters or more", Toast.LENGTH_SHORT).show();
 
-                }
-                else
-                {
+                } else if (binding.createUserField.getText().toString().equals("Select Field")) {
+                    Toast.makeText(getApplicationContext(), "Please select a field", Toast.LENGTH_SHORT).show();
+                } else {
                     user.setUserEmail(binding.createUserEmail.getText().toString().trim());
                     user.setUserPass(binding.createUserPass.getText().toString().trim());
                     user.setUserName(binding.crateUserName.getText().toString().trim());
 
+                    Toast.makeText(NewSignUp.this, user.getUserDept(), Toast.LENGTH_SHORT).show();
                     //register user
-                    firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
 
-                            if(task.isSuccessful()){
-                                Toast.makeText(getApplicationContext(),"Resgistration Succesfull",Toast.LENGTH_SHORT).show();
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Resgistration Succesfull", Toast.LENGTH_SHORT).show();
                                 sendEmailVerification();
                                 user.setFireuserid(firebaseAuth.getUid());
                                 updateUser(user);
 
-                            }
-                            else{
-                                Toast.makeText(getApplicationContext(),"Resgistration Unsuccesfull",Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Resgistration Unsuccesfull", Toast.LENGTH_SHORT).show();
                             }
 
                         }
@@ -102,19 +109,17 @@ public class NewSignUp extends AppCompatActivity {
     //send verification email
     private void sendEmailVerification() {
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        if(firebaseUser != null)
-        {
+        if (firebaseUser != null) {
             firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
-                    Toast.makeText(getApplicationContext(),"Verification e-mail Sent",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Verification e-mail Sent", Toast.LENGTH_SHORT).show();
                     firebaseAuth.signOut();
 
                 }
             });
-        }
-        else{
-            Toast.makeText(getApplicationContext(),"Failed to send verification e-mail",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Failed to send verification e-mail", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -124,12 +129,11 @@ public class NewSignUp extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
 
-                if(task.isSuccessful()){
-                    Toast.makeText(getApplicationContext(),"user is in database",Toast.LENGTH_SHORT).show();
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "user is in database", Toast.LENGTH_SHORT).show();
                     Log.d("result", "onComplete: " + task.getException());
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"user is not add in database",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "user is not add in database", Toast.LENGTH_SHORT).show();
                 }
 
             }
