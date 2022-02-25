94202a0b7951cfd0ef3ae9206bf2b728f4696551
Rohit Jagdale <rohitjagadale4@gmail.com>
2022-02-25 22:05:22 +0530

    made sign in ui

---
+++ app/src/main/java/com/taskmanager/horkrux/AuthNew/NewSignUp.java
@@ -0,0 +1,16 @@
+package com.taskmanager.horkrux.AuthNew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.taskmanager.horkrux.R;
+
+public class NewSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_sign_up);
+    }
+}
