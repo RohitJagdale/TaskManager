2f10f0458c13f1a6742f019ca49f461e208f9733
MusheerAhmed <musheerjamadar1024@gmail.com>
2022-02-25 22:05:22 +0530

    added 3 dot

--- app/src/main/java/com/taskmanager/horkrux/AdminPanel/AdminPanelActivity.java
+++ app/src/main/java/com/taskmanager/horkrux/AdminPanel/AdminPanelActivity.java
@@ -3,13 +3,16 @@ package com.taskmanager.horkrux.AdminPanel;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.taskmanager.horkrux.Activites.AssignTaskActivity;
 import com.taskmanager.horkrux.Activites.MainActivity;
 import com.taskmanager.horkrux.Models.Users;
+import com.taskmanager.horkrux.R;
 import com.taskmanager.horkrux.databinding.ActivityAdminPanelBinding;
 
 public class AdminPanelActivity extends AppCompatActivity {
@@ -31,6 +34,34 @@ public class AdminPanelActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(context, MainActivity.class));
+            }
+        });
+
+        binding.adminMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(context, binding.adminMenu);
+                popupMenu.getMenuInflater().inflate(R.menu.admin_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+
+                        if (menuItem.getItemId() == R.id.createTask) {
+                            Intent intent = new Intent(context, AssignTaskActivity.class);
+                            startActivity(intent);
+                            return true;
+                        }
+                        if (menuItem.getItemId() == R.id.adminUserView) {
+                            startActivity(new Intent(context, MainActivity.class));
+                            return true;
+                        }
+                        return false;
+
+
+                    }
+                });
+                // Showing the popup menu
+                popupMenu.show();
             }
         });
         binding.teamAndroid.setOnClickListener(new View.OnClickListener() {
