975923ce63977a473d6deb7c9c187c69cd0ff058
MusheerAhmed <musheerjamadar1024@gmail.com>
2022-02-26 12:42:18 +0530

    added update username

--- app/src/main/java/com/taskmanager/horkrux/Activites/Profile.java
+++ app/src/main/java/com/taskmanager/horkrux/Activites/Profile.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,6 +32,7 @@ public class Profile extends AppCompatActivity {
     private FirebaseDatabase database;
     private Context context = Profile.this;
     private String USER_PATH;
+    private String m_Text;
 
 
     @Override
@@ -46,6 +52,46 @@ public class Profile extends AppCompatActivity {
         binding.todoCount.setText(String.valueOf(MainActivity.count.getTodo()));
         binding.inProgressCount.setText(String.valueOf(MainActivity.count.getInProgress()));
         binding.doneCount.setText(String.valueOf(MainActivity.count.getDone()));
+
+        binding.editUserName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Enter User Name");
+
+
+                final EditText input = new EditText(context);
+
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        m_Text = input.getText().toString();
+                        if (m_Text.isEmpty()) {
+                            Toast.makeText(context, "Username cant be empty!!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            database.getReference(USER_PATH).child("userName").setValue(m_Text).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(context, "Username updated successfully", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+            }
+        });
 
         binding.backButton.setOnClickListener(new View.OnClickListener() {
             @Override
